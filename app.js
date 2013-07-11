a8c7fe3a515d0fb8b2e753b7ee04260cecf0d3e9
Tyler Stalder <tyler@stalder.me>
2013-07-10 17:34:59 -0700

    Initial commit of simple fitbit proxy.
    So far only handles auth and transforms fitbit response into a
    statusboard friendly json object for their graph widget.

---
+++ app.js
@@ -0,0 +1,95 @@
+var flatiron = require('flatiron'),
+    path = require('path'),
+    plates = require('plates'),
+    app = flatiron.app,
+    request = require('request'),
+    qs = require('querystring');
+
+var CONSUMER_KEY = process.env.FITBIT_KEY;
+var CONSUMER_SECRET = process.env.FITBIT_SECRET;
+
+app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
+
+app.use(flatiron.plugins.http);
+
+app.router.get('/', function () {
+  this.res.json({ 'hello': 'world' });
+});
+
+var oauth = {
+  callback: process.env.AUTH_CALLBACK,
+  consumer_key: CONSUMER_KEY,
+  consumer_secret: CONSUMER_SECRET
+};
+
+app.router.get('/auth', function() {
+  var res = this.res;
+  var url = 'https://api.fitbit.com/oauth/request_token';
+
+  request.post({url:url, oauth:oauth}, function (e, r, body) {
+
+    var access_token = qs.parse(body);
+
+    oauth = {
+      consumer_key: CONSUMER_KEY,
+      consumer_secret: CONSUMER_SECRET,
+      token: access_token.oauth_token
+    };
+
+    var url = 'https://www.fitbit.com/oauth/authorize?';
+
+    url += 'oauth_token=' + access_token.oauth_token;
+
+    var html = '<a href="/">Connect to Fitbit</a>';
+
+    var data = { "authUrl": url};
+    var map = plates.Map();
+
+    map.where('href').is('/').insert('authUrl');
+
+    var output = plates.bind(html, data, map);
+
+    res.end(output);
+  });
+});
+
+app.router.get('/auth/fitbit', function() {
+  var url = 'https://api.fitbit.com/oauth/access_token';
+  var res = this.res;
+
+  var access_token = this.req.query;
+
+  oauth = {
+    consumer_key: CONSUMER_KEY,
+    consumer_secret: CONSUMER_SECRET,
+    token: access_token.oauth_token,
+    verifier: access_token.oauth_verifier
+  };
+
+  request.post({url:url, oauth:oauth}, function (e, r, body) {
+
+    var perm_token = qs.parse(body);
+
+    oauth = {
+      consumer_key: CONSUMER_KEY,
+      consumer_secret: CONSUMER_SECRET,
+      token: perm_token.oauth_token,
+      token_secret: perm_token.oauth_token_secret
+    };
+
+    url = 'http://api.fitbit.com/1/user/-/activities/steps/date/today/7d.json';
+    request.get({url:url, oauth:oauth, json:true}, function (e, r, user) {
+      res.end(JSON.stringify(user));
+    });
+  });
+});
+
+app.router.get('/fitbit', function() {
+  var res = this.res;
+  var url = 'http://api.fitbit.com/1/user/-/activities/steps/date/today/7d.json';
+  request.get({url:url, oauth:oauth, json:true}, function (e, r, user) {
+    res.end(JSON.stringify(user));
+  });
+});
+
+app.start(process.env.PORT);
